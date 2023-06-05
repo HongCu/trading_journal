bcc5ca8a6b36eafdc7092b647b1ecc16e481a564
HongCu <timeors330@gmail.com>
2023-06-05 19:33:19 +0900

    [feat] save news if search a stock

--- src/main/java/com/stock/app/controller/ScrapController.java
+++ src/main/java/com/stock/app/controller/ScrapController.java
@@ -1,33 +1,37 @@
 package com.stock.app.controller;
 
+import com.stock.app.dto.BoardDto;
 import com.stock.app.entity.News;
 import com.stock.app.repository.NewsRepository;
 import com.stock.app.service.ScraperService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping(value = "/trading")
+@RequestMapping(value = "/stock")
 
 public class ScrapController {
     private final ScraperService scraperService;
     private final NewsRepository newsRepository;
 
+
     @GetMapping("/scrap")
-    public ResponseEntity<Map<String, Object>> scrapNews(@RequestParam String query) throws Exception {
+    public String scrapNews(@RequestParam String query) throws Exception {
 
         String[] fields = {"title", "originallink", "description"};
-        Map<String, Object> data = scraperService.scrap(query, fields);
+        scraperService.scrap(query, fields);
 
-        return ResponseEntity.status(HttpStatus.OK).body(data);
+        return "redirect:/";
     }
 
+    
     @GetMapping("/all")
     public ResponseEntity<List<News>> getAllData() {
         List<News> dataList = newsRepository.findAll();
