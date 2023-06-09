995f17019145178d9a6f6bbb0861756caede9065
HongCu <timeors330@gmail.com>
2023-06-09 19:15:41 +0900

    [feat] Checked Exception to Unchecked Exception

---
+++ src/main/java/com/stock/app/exception/DuplicateKeyException.java
@@ -0,0 +1,15 @@
+package com.stock.app.exception;
+
+public class DuplicateKeyException extends DbException {
+    public DuplicateKeyException() {
+    }
+    public DuplicateKeyException(String message) {
+        super(message);
+    }
+    public DuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public DuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
